27f067fc4cca5e31e9e64b0a4b73fece4be9a816
Juan LB <ljuan7397@gmail.com>
2024-07-09 18:28:22 +0200

    refactor: Remove unused RabbitMqModule imports in app.module.ts and user.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,30 +3,20 @@ import { UserModule } from '@modules/user/user.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import rabbitMqConfig from './config/rabbit-mq.config';
-import { EventBusPortSymbol } from '@lib';
-import { RabbitMqModule, RabbitMqEventBus } from '@config';
-import { CqrsModule } from '@nestjs/cqrs';
 import { SagasModule } from './sagas/sagas.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			envFilePath: '.env',
-			// envFilePath: `.env.${process.env.NODE_ENV}`,
 			isGlobal: true,
 			load: [rabbitMqConfig],
 		}),
 		SagasModule,
-		RabbitMqModule,
 		UserModule,
 		AuthModule,
 	],
 	controllers: [],
-	providers: [
-		{
-			provide: EventBusPortSymbol,
-			useClass: RabbitMqEventBus,
-		},
-	],
+	providers: [],
 })
 export class AppModule {}
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,13 +3,12 @@ import { PrismaModule } from '@prisma/prisma.module';
 import { UserController } from './presentation/user.controller';
 import { UserRepositoryPortSymbol } from './domain/user.repository.port';
 import { UserPrismaRepository } from './infrastructure/user.prisma.repository';
-import { RabbitMqModule } from '@config';
 import { CreateUserUseCase } from './application/use-case/create-user/create-user.use-case';
 import { ConfigModule } from '@nestjs/config';
 import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
-	imports: [PrismaModule, ConfigModule, RabbitMqModule, CqrsModule],
+	imports: [PrismaModule, ConfigModule, CqrsModule],
 	controllers: [UserController],
 	providers: [
 		CreateUserUseCase,
